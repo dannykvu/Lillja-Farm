eb8221f3b6b947ea48f93717b07ed0c7c40019b3
Danny Vu <contactdannyvu@gmail.com>
2025-05-17 23:30:08 -0500

    centering contact section

--- lillja-farm-app/pages/contact.tsx
+++ lillja-farm-app/pages/contact.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/Contact.module.css";
 import StoreHours from "./StoreHours";
 
 const Contact: NextPage = () => (
-  <div className="max-w-7xl px-4 sm:px-6 lg:px-8">
+  <div>
     <div id="contact" className="py-12">
       <h1
         className={`flex justify-center text-4xl font-extrabold text-gray-900 mb-6 ${styles.contactTitle}`}
